7e956a14ba3bee5e5864e625b023bf2ba3fa932b
DikiFaturrohman <dikifaturrohman17@gmai.com>
2023-05-04 19:03:42 +0700

    Work in Progress

---
+++ kuliah/pertemuan9/latihan0/index.php
@@ -0,0 +1,15 @@
+<?php
+$students = [
+  [
+    "nama" => "Diki Faturrohman",
+    "npm" => "2230404117",
+    "email" => "dikifaturrohman17@gmail.com"
+  ],
+  [
+    "nama" => "Afgiansyah",
+    "npm" => "223040119",
+    "email" => "afgi.223040119@mail.unpas.ac.id"
+  ]
+];
+
+require('index.view.php');
---
+++ kuliah/pertemuan9/latihan2/index.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'Home';
+$students = [
+  [
+    "nama" => "Diki",
+    "npm" => "043040023",
+    "email" => "diki.223040117h@unpas.ac.id"
+  ],
+  [
+    "nama" => "Afgy",
+    "npm" => "223040119",
+    "email" => "afgy@gmail.com"
+  ]
+];
+
+require('views/index.view.php');
